a558658df6f0e93f434a70aa75b543bd148e520a
AzureQDragon <36148173+AzureQDragon@users.noreply.github.com>
2018-10-09 22:18:18 -0400

    Reorganizing with OOP

--- MainApp.py
+++ MainApp.py
@@ -7,37 +7,11 @@ root = tk.Tk()
 #        size=36, weight='bold')
 #parallel lists
 
-elementNames = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon",
-            "Nitrogen", "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium",
-            "Aluminium", "Silicon", "Phosphorus", "Sulfur", "Chlorine", "Argon",
-            "Potassium", "Calcium", "Scandium", "Titainiium", "Vanadium", "Chromium",
-            "Manganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc", "Gallium",
-            "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium",
-            "Strontium", "Yttrium", "Zirconium", "Niobium", "Molybdenum", "Technetium",
-            "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin",
-            "Antimony", "Tellurium", "Iodine", "Xenon", "Caesium", "Barium", "Lanthanum",
-            "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europium",
-            "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium",
-            "Ytterbium", "Lutetium", "Hafnium", "Tantalum", "Tungsten", "Rhenium",
-            "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead",
-            "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium",
-            "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium",
-            "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium",
-            "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium",
-            "Bohrium", "Hassium", "Meitnerium", "Darmstadtium", "Roentgenium", "Copernicium",
-            "Nihonium", "Flerovium", "Moscovium", "Livermorium", "Tennessine", "Organesson"]
-atomicNumber = []
-for i in range(1, 119):
-    atomicNumber.append(i)
 
-atomicMass = [1.008, 4.0026, 6.94, 9.0122, 10.81, 12.011, 13.007, 15.999, 18.998,
-              20.180, 22.990, 24.305, 26.982, 28.085, 30.974, 32.06, ]
 
-funFacts = []
 
-score = 0
 
-questionPool = [atomicNumber, atomicMass, funFacts]
+
 currentQuestion = ""
 currentAnswer = []
 randomElement = 0
@@ -53,6 +27,34 @@ class Application(tk.Frame):
         self.master =  master
         self.grid(sticky=tk.E)
         self.createWidgets()
+        self.atomicMass = [1.008, 4.0026, 6.94, 9.0122, 10.81, 12.011, 13.007, 15.999, 18.998,
+                      20.180, 22.990, 24.305, 26.982, 28.085, 30.974, 32.06, ]
+        self.currentAnswer = ''
+        self.funFacts = []
+        self.score = 0
+        self.elementNames = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon",
+                    "Nitrogen", "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium",
+                    "Aluminium", "Silicon", "Phosphorus", "Sulfur", "Chlorine", "Argon",
+                    "Potassium", "Calcium", "Scandium", "Titainiium", "Vanadium", "Chromium",
+                    "Manganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc", "Gallium",
+                    "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium",
+                    "Strontium", "Yttrium", "Zirconium", "Niobium", "Molybdenum", "Technetium",
+                    "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin",
+                    "Antimony", "Tellurium", "Iodine", "Xenon", "Caesium", "Barium", "Lanthanum",
+                    "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europium",
+                    "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium",
+                    "Ytterbium", "Lutetium", "Hafnium", "Tantalum", "Tungsten", "Rhenium",
+                    "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead",
+                    "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium",
+                    "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium",
+                    "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium",
+                    "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium",
+                    "Bohrium", "Hassium", "Meitnerium", "Darmstadtium", "Roentgenium", "Copernicium",
+                    "Nihonium", "Flerovium", "Moscovium", "Livermorium", "Tennessine", "Organesson"]
+        self.atomicNumber = []
+        self.questionPool = [self.atomicNumber, self.atomicMass, self.funFacts]
+        for i in range(1, 119):
+            self.atomicNumber.append(i)
         self.Correct = False
         #self.total_label_text = tk.Text()
         #self.total_label = tk.Label(master, textvariable=self.Correct)
@@ -60,20 +62,22 @@ class Application(tk.Frame):
         vcmd = master.register(self.validate) # we have to wrap the command
         self.user_input = tk.Entry(master, validate="key", validatecommand=(vcmd, '%P'))
         self.userAnswer = ''
-        self.answer = tk.Button(self, text='Answer', command=self.checkAnswer)
-        self.answer.grid(row=2, column=1,
+        self.currentAnswerint = 0
+        self.answerbutton = tk.Button(self, text='Answer', command=self.checkAnswer)
+        self.answerbutton.grid(row=2, column=1,
         sticky=tk.E)
         #layout
         #self.label.grid(row=0, column=0, sticky=tk.W)
         self.user_input.grid(row=0, column=0)
         self.user_input.anchor()
+        
     def qAGen():
         # generates random "element", but actually the element's index, so (<atomicNumber> - 1)
         randomElement = random.randrange(118)
         # generates the actual element(the answer)
-        currentAnswer = atomicNumber[randomElement]
+        self.currentAnswerint = self.atomicNumber[randomElement]
         # generates the question using the randomElement index within one of the question pools
-        currentQuestion = random.choice(questionPool)[randomElement]
+        self.currentAnswer = random.choice(questionPool)[randomElement]
     def validate(self, new_input):
         if not new_input: # the field is being cleared
             self.userAnswer = ''
@@ -88,8 +92,9 @@ class Application(tk.Frame):
             return False
 
     def checkAnswer(self):
-        # just checks if the user's answer is the same as the actual answer (currentAnswer =? userAnswer)
-        if self.userAnswer.lower() == 'd':#self.currentAnswer:
+        # just checks if the user's answer is the same as the actual answer (currentAnswer =? userAnswer)\
+        print(self.currentAnswerint)
+        if self.userAnswer.lower() == self.currentAnswer:
             print("true")
             return True
         else:# self.userAnswer.lower() !='md':# self.currentAnswer:
@@ -104,7 +109,6 @@ class Application(tk.Frame):
         top.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)   # HEY DAN! I don't know what all this tkinter nonsense means but make sure that the variable
         self.columnconfigure(0, weight=1)  # you use for user input transfers to the variable named "userAnswer" in my functions
-
         label = tk.Label(self,
             anchor="w",fg="white",bg="blue")
 
